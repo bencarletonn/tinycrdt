dc0cbe5f15ec6977b8d6747ab3ee81cdb6b6a69b
Ben Carleton <ben.carleton@outlook.com>
2025-10-13 21:12:10 +0100

    Model trait boundaries and API surface based on functional requirements

--- src/doc.rs
+++ src/doc.rs
@@ -6,7 +6,9 @@ pub struct Doc<R: ConflictResolver> {
     pub client_id: u64,
     pub clock: u64,
     pub items: HashMap<ID, Item>,
+    pub pending: Vec<Item>,
     pub state_vector: StateVector,
+    pub head: Option<ID>,
     pub resolver: R,
 }
 
@@ -16,24 +18,32 @@ impl<R: ConflictResolver> Doc<R> {
             client_id,
             clock: 0,
             items: HashMap::new(),
+            pending: Vec::new(),
             state_vector: HashMap::new(),
+            head: Option::None,
             resolver,
         }
     }
+
+    fn next_id(&mut self) -> ID {}
+    fn find_pos(&self, pos: usize) -> (Option<ID>, Option<ID>) {}
+    fn try_link(&mut self, item: Item) {}
+    fn link(&mut self, item: Item) {}
+    fn resolve_pending(&mut self) {}
 }
 
-// TODO
+// impl Iter 
+
 impl<R: ConflictResolver> Crdt for Doc<R> {
     type Update = Vec<Item>;
 
     fn apply(&mut self, update: Self::Update) {}
     fn diff(&self, remote: &StateVector) -> Self::Update {}
-    fn state_vector(&self) -> StateVector {}
+    fn state_vector(&self) -> StateVector { self.state_vector.clone() }
 }
 
-// TODO
 impl<R: ConflictResolver> SequenceCrdt for Doc<R> {
-    fn insert(&mut self, pos: usize, content: &str) {}
+    fn insert(&mut self, pos: usize, text: &str) {}
     fn delete(&mut self, pos: usize, len: usize) {}
     fn value(&self) -> String {}
 }
